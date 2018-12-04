3ca4e27bbfd0caf4a5b9d314f9fa47765f0d3034
Matt Swanson <matt@mdswanson.com>
2018-12-03 22:23:27 -0500

    Day 03

---
+++ day03/part_two.rb
@@ -0,0 +1,70 @@
+file = File.open("day03/input.txt")
+lines = file.read.split("\n")
+
+class Claim < Struct.new(:id, :x, :y, :w, :h, :intact)
+  def overlaps?(idx)
+    cells.include? idx
+  end
+
+  def cells
+    @cells ||= build_cells
+  end
+
+  def build_cells
+    all = []
+    (0..w-1).each do |dx|
+      (0..h-1).each do |dy|
+        all << convert_coords(x + dx, y + dy)
+      end
+    end
+    all
+  end
+
+  def convert_coords(x, y)
+    x * 1000 + y
+  end
+end
+
+claims = []
+cells = Hash.new(0)
+size = 1000
+
+lines.each do |l|
+  parts = l.split(" ")
+  id = parts[0]
+  x, y = parts[2].split(",")
+  w, h = parts.last.split("x")
+  claims << Claim.new(id, x.to_i, y.to_i, w.to_i, h.to_i, true)
+end
+
+(0..size).each do |x|
+  cells[x] = Hash.new(0)
+  (0..size).each do |y|
+    cells[x][y] = 0
+  end
+end
+
+claims.each do |c|
+  puts "processing claim: #{c.id}"
+  (c.x..c.x+c.w-1).each do |x|
+    (c.y..c.y+c.h-1).each do |y|
+      cells[x][y] += 1
+    end
+  end
+end
+
+
+claims.each do |c|
+  puts "processing claim: #{c.id}"
+  (c.x..c.x+c.w-1).each do |x|
+    (c.y..c.y+c.h-1).each do |y|
+      if cells[x][y] > 1
+        c.intact = false
+      end
+    end
+  end
+end
+
+puts claims.select{|c| c.intact}.first
+
+
